ab6853cba3b1241d9c0d3106eda35dfe50ee6b45
Tanish Mittal <mittal.tanish@gmail.com>
2024-08-12 17:58:25 -0400

    Added all examples with comments for teachers

---
+++ src/lib/Title.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let titleText;
+</script>
+
+<!-- start with just title component, then teach export let -->
+<!-- <h1>Title Component</h1> -->
+
+<h1>Title: {titleText}</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Title from "$lib/Title.svelte";
+
+    //conditional rendering
+    let clicked = false;
+    function handleClicked() {
+        clicked = !clicked;
+    }
+
+    //each blocks
+    let fruits = ['apple', 'banana', 'grape', 'orange'];
+
+    //reactive state: first show without reactive, then with reactive
+    let count = 0;
+    $: doubled = count * 2;
+
+    //input binding, no example for dropdown
+    let name = '';
+    let boxChecked = true;
+
+</script>
+
+
+
+
+<main>
+    <!-- flexbox -->
+    <div class='parent'>
+        <p>Hello</p>
+        <p>World</p>
+    </div>
+
+    <!-- conditional rendering -->
+    <button on:click={handleClicked}>Conditionally Rendered</button>
+        {#if clicked}
+            <p>Button was clicked!</p>
+        {/if}
+  
+    <!-- each blocks -->
+    <ul>
+        <p>List of fruits:</p>
+        {#each fruits as fruit}
+            <li>{fruit}</li>
+        {/each}
+        
+        <!-- use to show index, and then put i+1 into li so t -->
+        <!-- {#each fruits as fruit, i}
+            <li>{i}. {fruit}</li>
+        {/each} -->
+    </ul>    
+
+    <!-- reactive state  -->
+    <button on:click={() => count += 1}>Reactive counter: {count}</button>
+    <p>doubled: {doubled}</p>
+
+    <!-- input binding  -->
+    <!-- text input, add conditional rendering after initially explaining  -->
+    <input bind:value={name} placeholder="Enter your name" />
+    {#if name}
+        <p>Hey {name}!</p>
+    {/if}
+    <br>
+    <!-- checkbox, first time seeing {:else} -->
+    <label> 
+        <input type='checkbox' bind:checked={boxChecked} />
+        Check me
+    </label>
+    {#if boxChecked}
+        <p>Box is checked!</p>
+    {:else}
+        <p>Box is not checked!</p>
+    {/if}
+
+    <!-- Title -->
+    <Title titleText="Our First Component"/>
+</main>
+
+<style>
+    /*flexbox*/
+    .parent {
+        /* display: flex; */
+        /* justify-content: space-between; */
+        /* justify-content: center;  */
+    }
+
+
+</style>
